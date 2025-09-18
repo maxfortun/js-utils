728a0d51ae24c70baf412c5869b5ce5f6e21019e
Max <max@maxf.net>
2025-09-17 22:35:18 -0400

    updated

--- index.test.js
+++ index.test.js
@@ -1,13 +1,14 @@
-const debug = require('debug')('js-utils-test');
+const debug = require('debug')('js-utils:test');
 
 const JSUtils = require('./index');
 
 const context = {
 	options: {
 		uris: [
-			`mongodb://localhost:47017/?directConnection=true`,
-			`mongodb://localhost:47018/?directConnection=true`,
+			'mongodb://localhost:47017/?directConnection=true',
+			'mongodb://localhost:47018/?directConnection=true',
 		],
+		rs_uri: 'mongodb://localhost:47017,localhost:47018/?directConnection=true&replicaSet=rs0',
 		options: {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
@@ -24,10 +25,11 @@ const context = {
 // jest.setTimeout(30000);
 
 describe('mongodb', () => {
-
-	test('mongodb_rs_uri', async () => {
+	test('rs_uri', async () => {
 		for(let uri of context.options.uris) {
-			await JSUtils.mongodb_rs_uri(uri, context.options.options);
+			const rs_uri = await JSUtils.mongodb_rs_uri(uri, context.options.options);
+			debug(uri, rs_uri);
+			expect(rs_uri).toEqual(context.options.rs_uri);
 		}
 	});
 
--- mongodb_rs_uri.js
+++ mongodb_rs_uri.js
@@ -11,45 +11,44 @@ const { URL } = require('url');
  * @returns {Promise<string>} - A corrected mongodb:// URI listing all members
  */
 module.exports = async function mongodb_rs_uri(uri, options) {
-  debug(uri, options);
+	const client = new MongoClient(uri, options);
+	try {
+		await client.connect();
 
-  const client = new MongoClient(uri, options);
-  try {
-    await client.connect();
+		// Run replSetGetConfig to discover replica set members
+		const adminDb = client.db('admin');
+		const result = await adminDb.command({ replSetGetConfig: 1 });
 
-    // Run replSetGetConfig to discover replica set members
-    const adminDb = client.db('admin');
-    const result = await adminDb.command({ replSetGetConfig: 1 });
+		const members = result.config.members.map(m => m.host);
 
-    const members = result.config.members.map(m => m.host);
+		// Parse original URI so we keep username, password, db, and options
+		const u = new URL(uri);
 
-    // Parse original URI so we keep username, password, db, and options
-    const u = new URL(uri);
+		const username = u.username ? decodeURIComponent(u.username) : null;
+		const password = u.password ? decodeURIComponent(u.password) : null;
+		const pathname = u.pathname; // includes leading /
+		const search = u.search;	 // includes leading ? (may already have replicaSet etc.)
 
-    const username = u.username ? decodeURIComponent(u.username) : null;
-    const password = u.password ? decodeURIComponent(u.password) : null;
-    const pathname = u.pathname; // includes leading /
-    const search = u.search;     // includes leading ? (may already have replicaSet etc.)
+		// If replicaSet is missing, preserve from config
+		const searchParams = new URLSearchParams(search);
+		if (!searchParams.has('replicaSet')) {
+			searchParams.set('replicaSet', result.config._id);
+		}
 
-    // If replicaSet is missing, preserve from config
-    const searchParams = new URLSearchParams(search);
-    if (!searchParams.has('replicaSet')) {
-      searchParams.set('replicaSet', result.config._id);
-    }
+		// Build new URI
+		let auth = '';
+		if (username) {
+			auth = encodeURIComponent(username);
+			if (password) auth += ':' + encodeURIComponent(password);
+			auth += '@';
+		}
 
-    // Build new URI
-    let auth = '';
-    if (username) {
-      auth = encodeURIComponent(username);
-      if (password) auth += ':' + encodeURIComponent(password);
-      auth += '@';
-    }
-
-    const newUri = `mongodb://${auth}${members.join(',')}${pathname}?${searchParams.toString()}`;
-    return newUri;
-  } finally {
-    await client.close();
-  }
+		const newUri = `mongodb://${auth}${members.join(',')}${pathname}?${searchParams.toString()}`;
+	debug(uri, '->', newUri);
+		return newUri;
+	} finally {
+		await client.close();
+	}
 }
 
 
